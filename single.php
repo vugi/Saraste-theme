e4976b557ed5a65de99f5e37d31f7fbac9bd17d8
Ville Vuorenmaa <vankka9@gmail.com>
2011-10-12 12:42:59 +0300

    Improved single.php

--- single.php
+++ single.php
@@ -9,10 +9,6 @@
 
 get_header(); ?>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-				<nav id="nav-above" class="navigation">
-					<?php previous_post_link( '%link', '' . _x( '&larr;', 'Previous post link', 'boilerplate' ) . ' %title' ); ?>
-					<?php next_post_link( '%link', '%title ' . _x( '&rarr;', 'Next post link', 'boilerplate' ) . '' ); ?>
-				</nav><!-- #nav-above -->
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<h1><?php the_title(); ?></h1>
 					<div class="entry-meta">
@@ -33,14 +29,13 @@ get_header(); ?>
 					</footer><!-- #entry-author-info -->
 <?php endif; ?>
 					<footer class="entry-utility">
-						<?php boilerplate_posted_in(); ?>
-						<?php edit_post_link( __( 'Edit', 'boilerplate' ), '<span class="edit-link">', '</span>' ); ?>
+						<?php edit_post_link('Muokkaa', '<span class="edit-link">', '</span>'); ?>
 					</footer><!-- .entry-utility -->
+					<nav id="nav-below" class="navigation">
+						<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'boilerplate' ) . '</span> %title' ); ?></div>
+						<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'boilerplate' ) . '</span>' ); ?></div>
+					</nav><!-- #nav-below -->
 				</article><!-- #post-## -->
-				<nav id="nav-below" class="navigation">
-					<div class="nav-previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'boilerplate' ) . '</span> %title' ); ?></div>
-					<div class="nav-next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'boilerplate' ) . '</span>' ); ?></div>
-				</nav><!-- #nav-below -->
 				<?php comments_template( '', true ); ?>
 <?php endwhile; // end of the loop. ?>
 <?php //get_sidebar(); ?>
