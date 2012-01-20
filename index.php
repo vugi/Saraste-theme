5f14b3db2518635486ebc9b3bd5d01edd22ad0d2
Ville Vuorenmaa <vankka9@gmail.com>
2012-01-20 16:01:59 +0200

    Featured box

--- index.php
+++ index.php
@@ -15,68 +15,13 @@
 
 get_header(); ?>
 
-<ul id="slides" class="kwicks">
-				<li class="slide">
-					<div id="post-16" class="post-16 post type-post status-publish format-standard hentry category-yleinen" style="background:url(<?php bloginfo( 'template_directory' ); ?>/images/slide-kajo.jpg) center no-repeat">
-						<div class="opacity"></div>
-						<a class="overlay" href="#" rel="bookmark"></a>
-						<div class="entry-container">
-							<h2 class="entry-title"><a href="#" title="Kajo" rel="bookmark">Kajo</a></h2>
-							<div class="entry-summary">
-								<p>Aamun Kajo, hän kauniina nousee, kasteella kulkee ja tanssahtelee.</p>
-								<div class="clear"></div>
-							</div><!-- .entry-summary -->
-						</div><!-- .entry-container -->
-					</div><!-- .post -->
-				</li>
-				<li class="slide">
-					<div id="post-12" class="post-12 post type-post status-publish format-standard hentry category-yleinen" style="background:url(<?php bloginfo( 'template_directory' ); ?>/images/slide-humu.jpg) center no-repeat">
-						<div class="opacity"></div>
-						<a class="overlay" href="#" rel="bookmark"></a>
-						<div class="entry-container">
-							<h2 class="entry-title"><a href="#" title="Humu" rel="bookmark">Humu</a></h2>
-							<div class="entry-summary">
-								<p>Astuu vierelleen Humu, jok&#8217; työhön ryhtyy, päivän askareet kannoillaan rytkyy.</p>
-								<div class="clear"></div>
-							</div><!-- .entry-summary -->
-						</div><!-- .entry-container -->
-					</div><!-- .post -->
-				</li>
-				<li class="slide">
-					<div id="post-9" class="post-9 post type-post status-publish format-standard hentry category-yleinen" style="background:url(<?php bloginfo( 'template_directory' ); ?>/images//slide-hamy.jpg) center no-repeat">
-						<div class="opacity"></div>
-						<a class="overlay" href="#" rel="bookmark"></a>
-						<div class="entry-container">
-							<h2 class="entry-title"><a href="#" title="Hämy" rel="bookmark">Hämy</a></h2>
-							<div class="entry-summary">
-								<p>Hämy illan iloisna luoksensa loikkii, ilta karnevaalit, ilot ja juhlat poikii!</p>
-								<div class="clear"></div>
-							</div><!-- .entry-summary -->
-						</div><!-- .entry-container -->
-					</div><!-- .post -->
-				</li>
-				<li class="slide">
-					<div id="post-6" class="post-6 post type-post status-publish format-standard hentry category-yleinen" style="background:url(<?php bloginfo( 'template_directory' ); ?>/images/slide-syvi.jpg) center no-repeat">
-						<div class="opacity"></div>
-						<a class="overlay" href="#" rel="bookmark"></a>
-						<div class="entry-container">
-							<h2 class="entry-title"><a href="#" title="Syvi" rel="bookmark">Syvi</a></h2>
-							<div class="entry-summary">
-								<p>Jälkeen heidän, maalle käynti saa suosion, yön Syvi ja levon hetki silloin suloisin on.</p>
-								<div class="clear"></div>
-							</div><!-- .entry-summary -->
-						</div><!-- .entry-container -->
-					</div><!-- .post -->
-				</li>
-			</ul><!-- #slides -->
-
-			<?php
-			/* Run the loop to output the posts.
-			 * If you want to overload this in a child theme then include a file
-			 * called loop-index.php and that will be used instead.
-			 */
-			 get_template_part( 'loop', 'index' );
-			?>
+<?php
+/* Run the loop to output the posts.
+ * If you want to overload this in a child theme then include a file
+ * called loop-index.php and that will be used instead.
+ */
+ get_template_part( 'loop', 'index' );
+?>
 
 <?php //get_sidebar(); ?>
 <?php get_footer(); ?>
---
+++ js/saraste-counter.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function () {
+	jQuery('#countdown').countdown({ until: new Date("March 23, 2012"), format: 'DHMS', layout: 
+								'<h3>Ilmoittautumisaikaa jäljellä</h3>' + 
+								'<div id="timer">' +
+									'<div id="timer_labels"><span>päivää</span><span>tuntia</span><span>minuuttia</span><span>sekuntia</span></div>' +
+									'<div id="timer_days" class="timer_numbers">{dnn}</div>'+
+									'<div id="timer_hours" class="timer_numbers">{hnn}</div>'+ 
+									'<div id="timer_mins" class="timer_numbers">{mnn}</div>'+
+									'<div id="timer_seconds" class="timer_numbers">{snn}</div>'+				
+								'</div>'
+								
+	})
+})
