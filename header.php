0bb7c8b426eb20b1743f1612b338ee77cf852f13
Antti Vuorela <antti.vuorela@iki.fi>
2012-01-21 18:49:23 +0200

    Removed background images & supersized library

--- header.php
+++ header.php
@@ -48,49 +48,9 @@
 		<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
 		<meta property="og:image" content="<?php bloginfo( 'template_directory' ); ?>/images/logo_facebook_like.gif"/>
 		
-		<script type="text/javascript" src="<?php bloginfo( 'template_directory' ); ?>/js/supersized.3.1.1.min.js"></script>
 		<script type='text/javascript' src='<?php bloginfo( 'template_directory' ); ?>/js/kwicks.js'></script>
 		<script type='text/javascript' src='<?php bloginfo( 'template_directory' ); ?>/js/saraste.js'></script>
-        
-    	<script type="text/javascript">  
-			jQuery(function($){
-				$.supersized({
-				
-					//Functionality
-					slideshow               :   1,		//Slideshow on/off
-					autoplay				:	1,		//Slideshow starts playing automatically
-					start_slide             :   1,		//Start slide (0 is random)
-					slide_interval          :   25000,	//Length between transitions
-					transition              :   1, 		//0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
-					transition_speed		:	1000,	//Speed of transition
-					new_window				:	0,		//Image links open in new window/tab
-					pause_hover             :   0,		//Pause slideshow on hover
-					keyboard_nav            :   1,		//Keyboard navigation on/off
-					performance				:	1,		//0-Normal, 1-Hybrid speed/quality, 2-Optimizes image quality, 3-Optimizes transition speed // (Only works for Firefox/IE, not Webkit)
-					image_protect			:	1,		//Disables image dragging and right click with Javascript
-					image_path				:	'img/', //Default image path
-
-					//Size + Position
-					min_width		        :   0,		//Min width allowed (in pixels)
-					min_height		        :   0,		//Min height allowed (in pixels)
-					vertical_center         :   1,		//Vertically center background
-					horizontal_center       :   1,		//Horizontally center background
-					fit_portrait         	:   1,		//Portrait images will not exceed browser height
-					fit_landscape			:   0,		//Landscape images will not exceed browser width
-					
-					//Components
-					navigation              :   0,		//Slideshow controls on/off
-					thumbnail_navigation    :   1,		//Thumbnail navigation
-					slide_counter           :   1,		//Display slide numbers
-					slide_captions          :   1,		//Slide caption (Pull from "title" in slides array)
-					slides 					:  	[		//Slideshow Images
-														{image : '<?php bloginfo( 'template_directory' ); ?>/images/Vaeltajia_KuvaJuhoVainio_1280px.jpg', title : '', url : ''},
-												]
-												
-				}); 
-		  });
-		    
-		</script>
+    
 	</head>
 	<body id="top" <?php body_class(); ?>>
 		<nav id="access" role="navigation">
