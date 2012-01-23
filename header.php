1da27b5087f5b8d59e521f09810338ca6d14dd6e
Antti Vuorela <antti.vuorela@iki.fi>
2012-01-23 02:23:08 +0200

    Random background color

--- header.php
+++ header.php
@@ -52,7 +52,10 @@
 		<script type='text/javascript' src='<?php bloginfo( 'template_directory' ); ?>/js/saraste.js'></script>
     
 	</head>
-	<body id="top" <?php body_class(); ?>>
+	<body id="top" <?php 
+	$arr = array("green", "blue", "yellow", "purple");
+	body_class("bg-".$arr[array_rand($arr)]);
+	?>>
 		<nav id="access" role="navigation">
             <a href="http://www.papa.partio.fi"><img src="<?php bloginfo( 'template_directory' ); ?>/images/PAPA_logo_rgb_web_transparent_100px.png" alt="Pääkaupunkiseudun Partiolaiset ry " style="float:right; padding: 5px 10px"/></a>
         </nav><!-- #access -->
