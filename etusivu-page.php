3662722946de98c7b978228936decca0b157b6d4
Antti Vuorela <antti.vuorela@iki.fi>
2012-01-25 00:51:39 +0200

    Removed some old code

--- etusivu-page.php
+++ etusivu-page.php
@@ -21,8 +21,6 @@ get_header();
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/featured.css">
 <!--[if lt IE 8]><style type="text/css">#countdown { display: none; }</style><![endif]-->
 
-
-
 <?php	$posts = get_posts(); ?>
 <?php	foreach($posts as $post) : setup_postdata($post); ?>
 		<article id="post-<?php the_ID(); ?>" <?php if($first) { echo ' class="first"'; } ?>>
--- js/saraste.js
+++ js/saraste.js
@@ -1,27 +1,4 @@
-function slide() {
-	jQuery('.kwicks').kwicks({
-		max : 455
-	});
-	jQuery('.slide').bind({
-		mouseenter: function() {
-			var slide = jQuery(this);
-			var opacity = jQuery('.opacity', slide);
-			var container = jQuery('.entry-container', slide);
-			opacity.stop(true, true).fadeOut(500);
-			container.stop(true, true).delay(500).fadeIn(500);
-		},
-		mouseleave: function() {
-			var slide = jQuery(this);
-			var opacity = jQuery('.opacity', slide);
-			var container = jQuery('.entry-container', slide);
-			opacity.stop(true, true).fadeIn(500);
-			container.stop(true, true).hide(500);
-		}
-	});
-}
-
 jQuery().ready(function() {
-	slide();
     jQuery("#fb").delay(1000).animate({bottom:'-200px'}, "slow");
     jQuery("#fb").hover(
       function () {
