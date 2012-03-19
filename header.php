af864741caa044ff676ec8afb80ad2b66ebc7361
Ville-Pekka Reponen <vpreponen@gmail.com>
2012-03-19 19:47:51 +0200

    Small fix to Partio logo css

--- header.php
+++ header.php
@@ -80,7 +80,7 @@
             <header role="banner">
                 <h1><a id="<?php echo $bg_color; ?>" href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                 <p><?php bloginfo( 'description' ); ?></p>
-                <a href="http://www.papa.partio.fi"><img src="<?php bloginfo( 'template_directory' ); ?>/images/PAPA_logo_rgb_web_transparent_100px.png" alt="Pääkaupunkiseudun Partiolaiset ry " style="float:right; padding: 5px 10px"/></a>
+                <a href="http://www.papa.partio.fi"><img src="<?php bloginfo( 'template_directory' ); ?>/images/PAPA_logo_rgb_web_transparent_100px.png" alt="Pääkaupunkiseudun Partiolaiset ry " style="display: block; margin: 10px auto;"/></a>
                 <p id="counter" style="display:none">Leiriin aikaa <b id="count"></b> päivää</p>
             </header>
 
