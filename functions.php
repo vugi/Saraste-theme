ce6c1c896f169c340fced22ad2901a0032fb915f
Ville Vuorenmaa <villevuor@gmail.com>
2012-05-28 23:43:21 +0300

    Bug fix...

--- functions.php
+++ functions.php
@@ -228,7 +228,14 @@ function purkit_comments( $comment, $args, $depth ) {
 //[alasivut]
 add_shortcode('alasivut', 'alasivut_func');
 function alasivut_func(){
-	$children = wp_list_pages("title_li=&child_of=" . $post->ID . "&echo=0");
+	global $post;
+
+	if($post->post_parent) {
+		$children = wp_list_pages("title_li=&child_of=" . $post->post_parent . "&echo=0");
+  } else {
+		$children = wp_list_pages("title_li=&child_of=" . $post->ID . "&echo=0");
+  }
+  
   if($children){
 		$r = '<ul>' . $children . '</ul>';
 		return $r;
