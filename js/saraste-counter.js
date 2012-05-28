986b80adbfa8b24e156829c9063599aa8846c036
Ville Vuorenmaa <villevuor@gmail.com>
2012-05-28 23:36:48 +0300

    Over 4500 :)

--- js/saraste-counter.js
+++ js/saraste-counter.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function () {
 	jQuery('#countdown').countdown({
 		until: new Date("July 30, 2012 10:00:00"),
 		format: 'DHMS',
-		layout: 'Lähes 4500 leiriläistä valtaa Evon {dnn} päivän, {hnn} tunnin, {mnn} minuutin ja {snn} sekunnin kuluttua.'		
+		layout: 'Yli 4500 leiriläistä valtaa Evon {dnn} päivän, {hnn} tunnin, {mnn} minuutin ja {snn} sekunnin kuluttua.'		
 	})
 })
